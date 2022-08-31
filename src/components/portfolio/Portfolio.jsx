b4bdf7b4d3b9284428f2daa216ff1f52eebbc7ae
kunal chauhan <kckr0115@gmail.com>
2022-08-30 23:51:29 -0700

    weather app add

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,6 +3,7 @@ import './Portfolio.css'
 import img1 from '../../assets/portfolio1.png'
 import img2 from '../../assets/portfolio2.png'
 import img3 from '../../assets/portfolio3.png'
+import img4 from '../../assets/weather-app.png'
 
 function Portfolio() {
   return (
@@ -40,6 +41,17 @@ function Portfolio() {
             <div className="portfolio__item-cta">
             <a href="https://github.com/kunalcode0/resturant-website" className="btn"  target="_blank"   rel="noreferrer">Github</a>
             <a href="https://kunalcode0.github.io/resturant-website/" className="btn btn-primary"  target="_blank"   rel="noreferrer">Live Demo</a>
+            </div>
+        </article>
+
+        <article className='portfolio__item'>
+            <div className="portfolio__image">
+                <img src={img4} alt="" />
+            </div>
+            <h3>Weather App</h3>
+            <div className="portfolio__item-cta">
+            <a href="https://github.com/kunalcode0/weather-app" className="btn"  target="_blank"   rel="noreferrer">Github</a>
+            <a href="https://kunalcode0.github.io/weather-app/" className="btn btn-primary"  target="_blank"   rel="noreferrer">Live Demo</a>
             </div>
         </article>
     </div>
